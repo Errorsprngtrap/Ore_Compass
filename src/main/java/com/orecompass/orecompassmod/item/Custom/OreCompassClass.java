80df0b0e13b30856a9ec2a29dbd441311015ae2f
Spring <baptistounet06@gmail.com>
2026-08-17 11:50:08 +0200

    m

--- src/main/java/com/orecompass/orecompassmod/item/Custom/OreCompassClass.java
+++ src/main/java/com/orecompass/orecompassmod/item/Custom/OreCompassClass.java
@@ -1,13 +1,16 @@
 package com.orecompass.orecompassmod.item.Custom;
 
+import com.orecompass.orecompassmod.tags.ModTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class OreCompassClass extends Item {
 
@@ -19,6 +22,11 @@ public class OreCompassClass extends Item {
     @Override
     public InteractionResult use(Level level, Player player, InteractionHand hand) {
         ItemStack itemStack = player.getItemInHand(hand);
+
+        if (level.isClientSide()){
+            return InteractionResult.SUCCESS;
+        }
+
         itemStack.hurtAndBreak(1, player, hand.asEquipmentSlot());
 
         BlockPos pos  = player.blockPosition();
@@ -26,11 +34,17 @@ public class OreCompassClass extends Item {
             for (int yy = - 20; yy <= 20; ++yy) {
                 for (int zz = - 20; zz <= 20; ++zz) {
                     System.out.println(xx + " " + yy + " " + zz);
+                    BlockPos blockpos = new BlockPos(xx, yy, zz);
+                    BlockState blockstate = level.getBlockState(blockpos);
+                    if (blockstate.is(ModTags.Blocks.IRON_COMPASS_DETECT)) {
+                        System.out.println("block found");
+                        return InteractionResult.SUCCESS;
+                    }
                 }
             }
         }
 
         System.out.println("Used");
-        return super.use(level, player, hand);
+        return InteractionResult.FAIL;
     }
 }
